ab2144aae4bb00b9ab4d99dad808a0dd7609018a
Levy <levymcruz@gmail.com>
2023-08-14 22:48:31 -0300

    left section finalized

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,15 +1,19 @@
 import styled from 'styled-components'
+import React from 'react';
+import Post from '../components/Post';
 
 
 export default function HomePage(){
     return (
         <Container>
             <Body>
-                Oii
+                <Post text={"Oia"} />
             </Body>
         </Container>
     )
 }
+
+
 
 const Container = styled.div`
     display: flex;
@@ -19,7 +23,7 @@ const Container = styled.div`
     width: 100%;
     min-height: 100vh;
     height: auto;
-	background-color: RGB(0, 26, 61);
+	background-color: #333333;
     /* padding-bottom: 5vh; */
     overflow-y: hidden;
 `;
@@ -32,10 +36,7 @@ const Body = styled.div`
 	gap: 2em;
 
     padding: 4em;
-    width: 100%;
+    width: 50%;
     height: 60%;
-    background-color: RGB(250, 250, 251);
-	box-shadow: 1px 1px 4px 4px rgba(170, 170, 170, 0.212); 
-    border-radius: 10px;
-    color: black;
+	
 `;
