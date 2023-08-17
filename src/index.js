9c84b8fc442f72f3955e8d15318518843e746d0e
João Vitor Diniz Mistrinel <abissaldev@gmail.com>
2023-08-17 20:36:23 -0300

    fix: immports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { ContextProvider } from "./components/Context";
-import App from "./components/App";
+import App from "./App";
 import Global from "./styles/Global";
 import ResetCSS from "./styles/Reset";
 
