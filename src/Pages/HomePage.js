3d899434228eee52f0e552f73764bac2b867480f
João Vitor Diniz Mistrinel <abissaldev@gmail.com>
2023-08-17 18:24:56 -0300

    fix: :bug:

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -33,6 +33,7 @@ export default function HomePage() {
 
   return (
     <Container>
+      <Header />
       <Body>
         {postsInfos ? (
           <>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
-import HomePage from "./Pages/HomePage";
 import { ContextProvider } from "./components/Context";
-import { EditPost } from "./components/editField/editField";
-import reportWebVitals from "./reportWebVitals";
+import App from "./components/App";
 import Global from "./styles/Global";
 import ResetCSS from "./styles/Reset";
 
