28cf4fe30a9117fe547029d540186b15dd038a2d
João Vitor Diniz Mistrinel <abissaldev@gmail.com>
2023-08-17 20:20:41 -0300

    fix: immports

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,14 +2,12 @@ import { useRoutes } from "react-router-dom";
 import HomePage from "../Pages/HomePage";
 import Signin from "../Pages/signin/Signin";
 import Signup from "../Pages/signup/Signup";
-import { EditPost } from "./components/editField/editField";
 
 const Routes = () => {
   return useRoutes([
     { path: "/", element: <Signin /> },
     { path: "/sign-up", element: <Signup /> },
     { path: "/timeline", element: <HomePage /> },
-    { path: "/editPost", element: <EditPost /> },
   ]);
 };
 export default Routes;
